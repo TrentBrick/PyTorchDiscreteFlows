344b5d490b30e908084a7c70b23c984a5e55ceef
Trenton Bricken <tbb21@duke.edu>
2020-07-03 20:05:28 +0100

    ported over Edward2 MADE network successfully

--- discrete_flows/made.py
+++ discrete_flows/made.py
@@ -1,17 +1,17 @@
+
 """
-Taken from https://github.com/karpathy/pytorch-normalizing-flows 
-Which was itself copy pasted from an earlier MADE implementation
-# https://github.com/karpathy/pytorch-made
-Implements a Masked Autoregressive MLP, where carefully constructed
-binary masks over weights ensure the autoregressive property.
+Masked autoencoder for distribution estimation.
+
+Code taken from Edward2: https://github.com/google/edward2/blob/master/edward2/tensorflow/layers/made.py
+and ported to PyTorch. 
+MaskedLinear taken from https://github.com/karpathy/pytorch-normalizing-flows and
+modified to work here. 
 """
 
 import numpy as np
 import torch
-import torch.nn.functional as F
 from torch import nn
-
-# NOTE: this is currently incompatible with onehot inputs!
+import torch.nn.functional as F
 
 class MaskedLinear(nn.Linear):
     """ same as Linear except has a configurable mask on the weights """
@@ -21,147 +21,237 @@ class MaskedLinear(nn.Linear):
         self.register_buffer('mask', torch.ones(out_features, in_features)) # creates a mask that is actually just all ones?? 
         
     def set_mask(self, mask): # called when the masks are created. passes in this mask. 
-        self.mask.data.copy_(torch.from_numpy(mask.astype(np.uint8).T)) 
+        #self.mask.data.copy_(torch.from_numpy(mask.astype(np.uint8).T)) 
+        #print('mask in set mask', mask, mask.shape)
+        mask = mask.long().T
+        self.mask.data.copy_(mask)
+        # if all of the inputs are zero, need to ensure the bias 
+        # is zeroed out!
+        self.bias_all_zero_mask = (mask.sum(dim=1)!=0).float()
         
     def forward(self, input):
-        return F.linear(input, self.mask * self.weight, self.bias)
+        return F.linear(input, self.mask * self.weight, self.bias_all_zero_mask * self.bias)
 
 class MADE(nn.Module):
-    def __init__(self, nin, hidden_sizes, nout, num_masks=1, natural_ordering=False):
-        """
-        nin: integer; number of inputs
-        hidden sizes: a list of integers; number of units in hidden layers
-        nout: integer; number of outputs, which usually collectively parameterize some kind of 1D distribution
-              note: if nout is e.g. 2x larger than nin (perhaps the mean and std), then the first nin
-              will be all the means and the second nin will be stds. i.e. output dimensions depend on the
-              
-              I THINK THIS IS DIFFERENT TO TRAN'S CODE. 
-              
-              same input dimensions in "chunks" and should be carefully decoded downstream appropriately.
-              the output of running the tests for this file makes this a bit more clear with examples.
-        num_masks: can be used to train ensemble over orderings/connections
-        natural_ordering: force natural ordering of dimensions, don't use random permutations
-        
-        * I dont get when other masks are made as there seems to be no iteration. 
-        * and when there is any shifting between the multiple masks that are requested.  
+    """Masked autoencoder for distribution estimation (Germain et al., 2015).
+    MADE takes as input a real Tensor of shape [..., length, channels] and returns
+    a Tensor of shape [..., length, units] and same dtype. It masks layer weights
+    to satisfy autoregressive constraints with respect to the length dimension. In
+    particular, for a given ordering, each input dimension of length can be
+    reconstructed from previous dimensions.
+    The output's units dimension captures per-time-step representations. For
+    example, setting units to 2 can parameterize the location and log-scale of an
+    autoregressive Gaussian distribution.
+    """
 
-        Seems to only build and use one mask, could use a lot more. 
-        Some of the weights will not be trained as a result
-        Allows for autoregressive flows that are feedforward in efficiency. 
-        
+    def __init__(self,
+                input_shape, 
+                units,
+                hidden_dims,
+                input_order='left-to-right',
+                hidden_order='left-to-right',
+                use_bias=True,
+                **kwargs):
+        """Constructs network.
+        Args:
+            units: Positive integer, dimensionality of the output space.
+            hidden_dims: list with the number of hidden units per layer. It does not
+                include the output layer; those number of units will always be set to
+                the input dimension multiplied by `num_heads`. Each hidden unit size
+                must be at least the size of length (otherwise autoregressivity is not
+                possible).
+            input_order: Order of degrees to the input units: 'random',
+                'left-to-right', 'right-to-left', or an array of an explicit order.
+                For example, 'left-to-right' builds an autoregressive model
+                p(x) = p(x1) p(x2 | x1) ... p(xD | x<D).
+            hidden_order: Order of degrees to the hidden units: 'random',
+                'left-to-right'. If 'left-to-right', hidden units are allocated equally
+                (up to a remainder term) to each degree.
+            activation: Activation function.
+            use_bias: Whether to use a bias.
+            **kwargs: Keyword arguments of parent class.
         """
-        
         super().__init__()
-        self.nin = nin
-        self.nout = nout
-        self.hidden_sizes = hidden_sizes
-        for h in hidden_sizes:
-            assert h >= nin, "Need more hidden units than input dims for autoregressive to hold!"
-        assert self.nout % self.nin == 0, "nout must be integer multiple of nin"
-        
-        # define a simple MLP neural net
-        self.net = []
-        hs = [nin] + hidden_sizes + [nout]
-        for h0,h1 in zip(hs, hs[1:]): # feeds from one layer to the next.
-            self.net.extend([
-                    MaskedLinear(h0, h1),
-                    nn.ReLU(),
-                ])
-        self.net.pop() # pop the last ReLU for the output layer
-        self.net = nn.Sequential(*self.net)
-        
-        # seeds for orders/connectivities of the model ensemble
-        self.natural_ordering = natural_ordering
-        self.num_masks = num_masks
-        self.seed = 0 # for cycling through num_masks orderings
-        
-        self.m = {}
-        self.update_masks() # builds the initial self.m connectivity
-        # note, we could also precompute the masks and cache them, but this
-        # could get memory expensive for large number of masks.
-        
-    def update_masks(self):
-        print('update masks is running!')
-        if self.m and self.num_masks == 1: # UNLESS THE DICTIONARY M IS MADE THEN IT WILL RUN FOR THE FIRST TIME.  
-            print('is not updating the masks at all')
-            return # only a single seed, skip for efficiency
-        L = len(self.hidden_sizes)
-        
-        # fetch the next seed and construct a random stream
-        rng = np.random.RandomState(self.seed) # made with a given random seed. 
-        self.seed = (self.seed + 1) % self.num_masks
-        
-        # sample the order of the inputs and the connectivity of all neurons
-        self.m[-1] = np.arange(self.nin) if self.natural_ordering else rng.permutation(self.nin) # m is a dictionary.
-        for l in range(L):
-            self.m[l] = rng.randint(self.m[l-1].min(), self.nin-1, size=self.hidden_sizes[l])
-        
-        # construct the mask matrices
-        masks = [self.m[l-1][:,None] <= self.m[l][None,:] for l in range(L)]
-        masks.append(self.m[L-1][:,None] < self.m[-1][None,:])
-        
-        # handle the case where nout = nin * k, for integer k > 1
-        # it is as if the same output connections are applied and repeated in chunks. eg same flow applied for the mean and then also for the std. 
-        if self.nout > self.nin:
-            k = int(self.nout / self.nin)
-            # replicate the mask across the other outputs
-            masks[-1] = np.concatenate([masks[-1]]*k, axis=1) # taking the last mask
-        
-        # set the masks in all MaskedLinear layers
-        layers = [l for l in self.net.modules() if isinstance(l, MaskedLinear)]
-        for l,m in zip(layers, masks):
-            print('masks are:', m, m.shape)
-            l.set_mask(m) # sets the mask for the different layers. 
-    
-    def forward(self, x):
-        print('madde shape input', x.shape)
-        return self.net(x)
+        self.units = int(units)
+        self.hidden_dims = hidden_dims
+        self.input_order = input_order
+        self.hidden_order = hidden_order
+        #self.activation = getattr(F, activation)
+        self.use_bias = use_bias
+        self.network = nn.ModuleList()
+        self.build(input_shape)
 
-#------------------
+    def build(self, input_shape):
+        length = input_shape[-2]
+        channels = input_shape[-1]
+        if length is None or channels is None:
+            raise ValueError('The two last dimensions of the inputs to '
+                                             '`MADE` should be defined. Found `None`.')
+        masks = create_masks(input_dim=length,
+                            hidden_dims=self.hidden_dims,
+                            input_order=self.input_order,
+                            hidden_order=self.hidden_order)
 
-if __name__ == '__main__':
-    from torch.autograd import Variable
-    
-    # run a quick and dirty test for the autoregressive property
-    D = 10
-    rng = np.random.RandomState(14)
-    x = (rng.rand(1, D) > 0.5).astype(np.float32)
-    print('the data x', x)
-    configs = [
-        (D, [], D, False),                 # test various hidden sizes
-        (D, [200], D, False),
-        (D, [200, 220], D, False),
-        (D, [200, 220, 230], D, False),
-        (D, [200, 220], D, True),          # natural ordering test
-        (D, [200, 220], 2*D, True),       # test nout > nin
-        (D, [200, 220], 3*D, False),       # test nout > nin
-    ]
-    
-    for nin, hiddens, nout, natural_ordering in configs:
+        '''print('masks made at start')
+        for m in masks: 
+            print(m.shape)'''
+        # Input-to-hidden layer: [..., length, channels] -> [..., hidden_dims[0]].
+        #self.network.append(tf.keras.layers.Reshape([length * channels]))
+        # Tile the mask so each element repeats contiguously; this is compatible
+        # with the autoregressive contraints unlike naive tiling.
+        mask = masks[0]
+        mask = mask.unsqueeze(1).repeat(1, channels, 1)
+        mask = mask.view(mask.shape[0] * channels, mask.shape[-1])
+        #print('hidden dims are:', self.hidden_dims)
+        if self.hidden_dims:
+            #print("in and out dims for first layer", channels*length,self.hidden_dims[0])
+            layer = MaskedLinear(channels*length,self.hidden_dims[0])
+            layer.set_mask(mask)
+
+            '''tf.keras.layers.Dense(
+                    self.hidden_dims[0],
+                    kernel_initializer=make_masked_initializer(mask),
+                    kernel_constraint=make_masked_constraint(mask),
+                    activation=self.activation,
+                    use_bias=self.use_bias)'''
+            self.network.append(layer)
+            self.network.append(nn.ReLU())
+
+        #print('made the first mask!', mask)
+        # Hidden-to-hidden layers: [..., hidden_dims[l-1]] -> [..., hidden_dims[l]].
+        for ind in range(1, len(self.hidden_dims)-1):
         
-        print("checking nin %d, hiddens %s, nout %d, natural %s" % 
-             (nin, hiddens, nout, natural_ordering))
-        model = MADE(nin, hiddens, nout, natural_ordering=natural_ordering)
-        
-        # run backpropagation for each dimension to compute what other
-        # dimensions it depends on.
-        res = []
-        for k in range(nout):
-            xtr = Variable(torch.from_numpy(x), requires_grad=True)
-            xtrhat = model(xtr)
-            #print('xtrhat', xtrhat.shape)
-            loss = xtrhat[0,k] # it is a matrix with 1 row. 
-            # by doing the loss on each of the points we can see what they are connected to
-            loss.backward()
-            
-            # having a nonzero gradient shows dependence. 
-            depends = (xtr.grad[0].numpy() != 0).astype(np.uint8)
-            depends_ix = list(np.where(depends)[0])
-            isok = k % nin not in depends_ix
-            
-            res.append((len(depends_ix), k, depends_ix, isok))
-        
-        # pretty print the dependencies
-        res.sort()
-        for nl, k, ix, isok in res:
-            print("output %2d depends on inputs: %30s : %s" % (k, ix, "OK" if isok else "NOTOK"))
+            layer = MaskedLinear(self.hidden_dims[ind],self.hidden_dims[ind+1])
+            layer.set_mask(masks[ind])
+
+            '''tf.keras.layers.Dense(
+                    self.hidden_dims[0],
+                    kernel_initializer=make_masked_initializer(mask),
+                    kernel_constraint=make_masked_constraint(mask),
+                    activation=self.activation,
+                    use_bias=self.use_bias)'''
+            self.network.append(layer)
+            self.network.append(nn.ReLU())
+
+        # Hidden-to-output layer: [..., hidden_dims[-1]] -> [..., length, units].
+        # Tile the mask so each element repeats contiguously; this is compatible
+        # with the autoregressive contraints unlike naive tiling.
+        if self.hidden_dims:
+            mask = masks[-1]
+        mask = mask.unsqueeze(-1).repeat(1, 1, self.units)
+        mask = mask.view(mask.shape[0], mask.shape[1] * self.units)
+        #print(self.units)
+        layer = MaskedLinear(self.hidden_dims[-1],channels*length)
+        layer.set_mask(mask)
+
+        self.network.append(layer)
+        #self.network.append(tf.keras.layers.Reshape([length, self.units]))
+        self.network = nn.Sequential(*self.network)
+
+    def forward(self, inputs):
+        #print('going foreward!!')
+        input_shapes = inputs.shape
+        inputs = inputs.view(-1, input_shapes[-1]*input_shapes[-2])
+        #for l in self.network:
+        #    inputs = l(inputs)
+        inputs = self.network(inputs)
+        out = inputs.view(-1, input_shapes[-2], self.units)
+        return out
+
+
+def create_degrees(input_dim,
+                    hidden_dims,
+                    input_order='left-to-right',
+                    hidden_order='left-to-right'):
+    """Returns a list of degree vectors, one for each input and hidden layer.
+    A unit with degree d can only receive input from units with degree < d. Output
+    units always have the same degree as their associated input unit.
+    Args:
+        input_dim: Number of inputs.
+        hidden_dims: list with the number of hidden units per layer. It does not
+            include the output layer. Each hidden unit size must be at least the size
+            of length (otherwise autoregressivity is not possible).
+        input_order: Order of degrees to the input units: 'random', 'left-to-right',
+            'right-to-left', or an array of an explicit order. For example,
+            'left-to-right' builds an autoregressive model
+            p(x) = p(x1) p(x2 | x1) ... p(xD | x<D).
+        hidden_order: Order of degrees to the hidden units: 'random',
+            'left-to-right'. If 'left-to-right', hidden units are allocated equally
+            (up to a remainder term) to each degree.
+    """
+    if (isinstance(input_order, str) and
+            input_order not in ('random', 'left-to-right', 'right-to-left')):
+        raise ValueError('Input order is not valid.')
+    if hidden_order not in ('random', 'left-to-right'):
+        raise ValueError('Hidden order is not valid.')
+
+    degrees = []
+    if isinstance(input_order, str):
+        input_degrees = np.arange(1, input_dim + 1)
+        if input_order == 'right-to-left':
+            input_degrees = np.flip(input_degrees, 0)
+        elif input_order == 'random':
+            np.random.shuffle(input_degrees)
+    else:
+        input_order = np.array(input_order)
+        if np.all(np.sort(input_order) != np.arange(1, input_dim + 1)):
+            raise ValueError('invalid input order')
+        input_degrees = input_order
+    degrees.append(input_degrees)
+
+    for units in hidden_dims:
+        if hidden_order == 'random':
+            min_prev_degree = min(np.min(degrees[-1]), input_dim - 1)
+            hidden_degrees = np.random.randint(
+                    low=min_prev_degree, high=input_dim, size=units)
+        elif hidden_order == 'left-to-right':
+            hidden_degrees = (np.arange(units) % max(1, input_dim - 1) +
+                                                min(1, input_dim - 1))
+        degrees.append(hidden_degrees)
+    return degrees
+
+
+def create_masks(input_dim,
+                hidden_dims,
+                input_order='left-to-right',
+                hidden_order='left-to-right'):
+    """Returns a list of binary mask matrices respecting autoregressive ordering.
+    Args:
+        input_dim: Number of inputs.
+        hidden_dims: list with the number of hidden units per layer. It does not
+            include the output layer; those number of units will always be set to
+            input_dim downstream. Each hidden unit size must be at least the size of
+            length (otherwise autoregressivity is not possible).
+        input_order: Order of degrees to the input units: 'random', 'left-to-right',
+            'right-to-left', or an array of an explicit order. For example,
+            'left-to-right' builds an autoregressive model
+            p(x) = p(x1) p(x2 | x1) ... p(xD | x<D).
+        hidden_order: Order of degrees to the hidden units: 'random',
+            'left-to-right'. If 'left-to-right', hidden units are allocated equally
+            (up to a remainder term) to each degree.
+    """
+    degrees = create_degrees(input_dim, hidden_dims, input_order, hidden_order)
+    masks = []
+    # Create input-to-hidden and hidden-to-hidden masks.
+    for input_degrees, output_degrees in zip(degrees[:-1], degrees[1:]):
+        mask = torch.Tensor(input_degrees[:, np.newaxis] <= output_degrees).float()
+        masks.append(mask)
+
+    # Create hidden-to-output mask.
+    mask = torch.Tensor(degrees[-1][:, np.newaxis] < degrees[0]).float()
+    masks.append(mask)
+    return masks
+
+
+'''def make_masked_initializer(mask):
+    initializer = tf.keras.initializers.GlorotUniform()
+    def masked_initializer(shape, dtype=None):
+        return mask * initializer(shape, dtype)
+    return masked_initializer
+
+
+def make_masked_constraint(mask):
+    constraint = tf.identity
+    def masked_constraint(x):
+        return mask * constraint(x)
+    return masked_constraint'''
